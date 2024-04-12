a97b36fef620f34158e61461b745b54644a743c5
samirmirzaev <xsamirx-1@mail.ru>
2024-04-13 00:25:07 +0500

    blya

--- handlers.py
+++ handlers.py
@@ -1,9 +1,72 @@
-from aiogram import types, F, Router
-from aiogram.types import Message
-from aiogram.filters import Command
+# from aiogram import F, Router, types
+# from aiogram.filters import Command, Text
+# from aiogram.fsm.context import FSMContext
+#
+# router = Router()
+#
+# @router.message(Command("start"))
+# async def main(message: types.Message):
+#     await message.reply('Привет!')
+#
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# @router.message(Command("add_to_list"))
+# async def cmd_add_to_list(message: types.Message):
+#     await message.answer("Введите число:")
+#     @router.message()
+#     async def add_to_list(message: types.Message):
+#         print(message.text)
+#         try:
+#             sas = int(message.text)
+#             print(sas)
+#         except ValueError:
+#             await message.answer("Некорректный ввод. Пожалуйста, введите число.")
+#             return
+#
+#         mylist.append(sas)
+#         await message.answer(f"Ваш список: {mylist}")
+#
+# @router.message(Command("show_list"))
+# async def cmd_show_list(message: types.Message):
+#     await message.answer(f"Ваш список: {mylist}")
 
+
+
+
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, CallbackQuery
+
+
+class Statemenu(StatesGroup):
+    add_to_list = State()
+    show_list = State()
+
+mylist = []
 router = Router()
 
-@router.message(Command("start"))
-async def start_handler(msg: Message):
-    await msg.answer("Helloт!")
+
+@router.message(CommandStart())
+async def start_command(message: Message, state: FSMContext):
+    await message.answer('Hello')
+    await state.set_state(Statemenu.add_to_list)
+
+@router.message(Command("add_to_list"))
+async def add_to_list(message: Message, state: FSMContext):
+    print(message.text)
+    await message.answer('введите число:')
+    sas = int(message.text)
+    print(sas)
+    try:
+        mylist.append(sas)
+    except ValueError:
+        await message.answer("Некорректный ввод. Пожалуйста, введите число.")
+    finally:
+        await message.answer(f"Ваш список: {mylist}")
+
+@router.message(F.text =='/show_list')
+async def show_list(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(f"Ваш списокc: {mylist}")
---
+++ kb.py
@@ -0,0 +1,23 @@
+from aiogram.types import  InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+menu = [
+    [InlineKeyboardButton(text="📝 Генерировать текст", callback_data="generate_text"),
+    InlineKeyboardButton(text="🖼 Генерировать изображение", callback_data="generate_image")],
+    [InlineKeyboardButton(text="💳 Купить токены", callback_data="buy_tokens"),
+    InlineKeyboardButton(text="💰 Баланс", callback_data="balance")],
+    [InlineKeyboardButton(text="💎 Партнёрская программа", callback_data="ref"),
+    InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
+    [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
+]
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Выход в меню")]], resize_keyboard=True)
+iexeit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Выход в меню", callback_data="menu")]])
+
+
+# second variant to create buttons
+
+# builder = InlineKeyboardBuilder()
+# for i in range(15):
+#     builder.button(text=f"Кнопка {i}", callback_data=f"button_{i}")
+# builder.adjust(2)
+# await msg.answer("Текст сообщения", reply_markup=builder.as_markup())
